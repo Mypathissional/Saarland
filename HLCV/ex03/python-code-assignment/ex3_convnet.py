6f5f9daf1431129dd10fc7f4269c8dbbea293433
maria sargsyan <maria7sargsyan@gmail.com>
2019-05-19 15:16:57 +0400

    ex1 init

--- HLCV/ex03/python-code-assignment/ex3_convnet.py
+++ HLCV/ex03/python-code-assignment/ex3_convnet.py
@@ -3,27 +3,30 @@ import torch.nn as nn
 import torchvision
 import torchvision.transforms as transforms
 import numpy as np
-
+from math import ceil
 import matplotlib.pyplot as plt
+
 
 def weights_init(m):
     if type(m) == nn.Linear:
         m.weight.data.normal_(0.0, 1e-3)
         m.bias.data.fill_(0.)
 
+
 def update_lr(optimizer, lr):
     for param_group in optimizer.param_groups:
         param_group['lr'] = lr
 
-#--------------------------------
-# Device configuration
-#--------------------------------
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-print('Using device: %s'%device)
 
-#--------------------------------
+# --------------------------------
+# Device configuration
+# --------------------------------
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+print('Using device: %s' % device)
+
+# --------------------------------
 # Hyper-parameters
-#--------------------------------
+# --------------------------------
 input_size = 3
 num_classes = 10
 hidden_size = [128, 512, 512, 512, 512, 512]
@@ -31,16 +34,16 @@ num_epochs = 20
 batch_size = 200
 learning_rate = 2e-3
 learning_rate_decay = 0.95
-reg=0.001
-num_training= 49000
-num_validation =1000
+reg = 0.001
+num_training = 49000
+num_validation = 1000
 norm_layer = None
 print(hidden_size)
 
 
-#-------------------------------------------------
+# -------------------------------------------------
 # Load the CIFAR-10 dataset
-#-------------------------------------------------
+# -------------------------------------------------
 #################################################################################
 # TODO: Q3.a Chose the right data augmentation transforms with the right        #
 # hyper-parameters and put them in the data_aug_transforms variable             #
@@ -49,49 +52,58 @@ data_aug_transforms = []
 # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
 # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
-norm_transform = transforms.Compose(data_aug_transforms+[transforms.ToTensor(),
-                                     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
-                                     ])
+norm_transform = transforms.Compose(data_aug_transforms + [transforms.ToTensor(),
+                                                           transforms.Normalize(
+                                                               (0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+                                                           ])
 test_transform = transforms.Compose([transforms.ToTensor(),
-                                     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+                                     transforms.Normalize(
+                                         (0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
                                      ])
 cifar_dataset = torchvision.datasets.CIFAR10(root='datasets/',
-                                           train=True,
-                                           transform=norm_transform,
-                                           download=False)
+                                             train=True,
+                                             transform=norm_transform,
+                                             download=False)
 
 test_dataset = torchvision.datasets.CIFAR10(root='datasets/',
-                                          train=False,
-                                          transform=test_transform
-                                          )
-#-------------------------------------------------
+                                            train=False,
+                                            transform=test_transform
+                                            )
+# -------------------------------------------------
 # Prepare the training and validation splits
-#-------------------------------------------------
+# -------------------------------------------------
 mask = list(range(num_training))
 train_dataset = torch.utils.data.Subset(cifar_dataset, mask)
 mask = list(range(num_training, num_training + num_validation))
 val_dataset = torch.utils.data.Subset(cifar_dataset, mask)
 
-#-------------------------------------------------
+# -------------------------------------------------
 # Data loader
-#-------------------------------------------------
+# -------------------------------------------------
 train_loader = torch.utils.data.DataLoader(dataset=train_dataset,
                                            batch_size=batch_size,
                                            shuffle=True)
 
 val_loader = torch.utils.data.DataLoader(dataset=val_dataset,
-                                           batch_size=batch_size,
-                                           shuffle=False)
+                                         batch_size=batch_size,
+                                         shuffle=False)
 
 test_loader = torch.utils.data.DataLoader(dataset=test_dataset,
                                           batch_size=batch_size,
                                           shuffle=False)
 
 
-#-------------------------------------------------
+# -------------------------------------------------
 # Convolutional neural network (Q1.a and Q2.a)
 # Set norm_layer for different networks whether using batch normalization
-#-------------------------------------------------
+# -------------------------------------------------
+
+class Flatten(nn.Module):
+    def forward(self, x):
+        # return x.view(x.size()[0], -1)
+        return x.reshape(-1, x.size()[1])
+
+
 class ConvNet(nn.Module):
     def __init__(self, input_size, hidden_layers, num_classes, norm_layer=None):
         super(ConvNet, self).__init__()
@@ -102,27 +114,53 @@ class ConvNet(nn.Module):
         # For Q2.a make use of BatchNorm2d layer from the torch.nn module.              #
         # For Q3.b Use Dropout layer from the torch.nn module.                          #
         #################################################################################
+
         layers = []
+
         # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
+        dims = [input_size]
+        dims.extend(hidden_layers)
+        self.input_size = input_size
 
+        for i in range(len(dims) - 1):
+            [hid_in, hid_out] = [dims[i], dims[i + 1]]
+            layers.append(self.__convBlock(hid_in, hid_out, norm_layer))
+
+        layers.append(Flatten())
+        layers.append(nn.Linear(hidden_layers[-1], num_classes))
+
+        self.net = nn.Sequential(*layers)
         # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
+
+    def __convBlock(self, in_channel, out_channel, norm_layer, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1)):
+        if norm_layer:
+            seq = nn.Sequential(nn.Conv2d(in_channel, out_channel, kernel_size, stride, padding),
+                                nn.BatchNorm2d(out_channel),
+                                nn.MaxPool2d(kernel_size=(
+                                    2, 2), stride=(2, 2)),
+                                nn.ReLU())
+        else:
+            seq = nn.Sequential(nn.Conv2d(in_channel, out_channel, kernel_size, stride, padding),
+                                nn.MaxPool2d(kernel_size=(
+                                    2, 2), stride=(2, 2)),
+                                nn.ReLU())
+        return seq
 
     def forward(self, x):
         #################################################################################
         # TODO: Implement the forward pass computations                                 #
         #################################################################################
         # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
-
-
+        out = self.net(x)
         # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
         return out
 
 
-#-------------------------------------------------
+# -------------------------------------------------
 # Calculate the model size (Q1.b)
 # if disp is true, print the model parameters, otherwise, only return the number of parameters.
-#-------------------------------------------------
+# -------------------------------------------------
 def PrintModelSize(model, disp=True):
     #################################################################################
     # TODO: Implement the function to count the number of trainable parameters in   #
@@ -130,52 +168,68 @@ def PrintModelSize(model, disp=True):
     # training                                                                      #
     #################################################################################
     # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
-
+    model_sz = 0
+    for i, param in enumerate(model.parameters()):
+        if param.requires_grad:
+            param_size = param.size()
+            model_sz += param_size.numel()
+            if disp:
+                print("{0}___param shape:  {1}, param_size:  {2}".format(
+                    map(str, [i, param.size(), param.size().numel()])))
 
     # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
     return model_sz
 
-#-------------------------------------------------
+# -------------------------------------------------
 # Calculate the model size (Q1.c)
 # visualize the convolution filters of the first convolution layer of the input model
-#-------------------------------------------------
-def VisualizeFilter(model):
+# -------------------------------------------------
+
+
+def VisualizeFilter(model, vert=20.):
     #################################################################################
     # TODO: Implement the functiont to visualize the weights in the first conv layer#
     # in the model. Visualize them as a single image fo stacked filters.            #
     # You can use matlplotlib.imshow to visualize an image in python                #
     #################################################################################
     # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
-
+    first_conv = next(model.parameters())
+    first_conv = first_conv.detach.numpy()
+    fig, axs = plt.subplots(ceil(first_conv.shape[0] / vert), vert)
+    for i in range(first_conv.shape[0] / vert):
+        for j in range(vert):
+            axs[i][j].imshow(first_conv[i * vert + j])
     # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
-#======================================================================================
-# Q1.a: Implementing convolutional neural net in PyTorch
-#======================================================================================
-# In this question we will implement a convolutional neural networks using the PyTorch
-# library.  Please complete the code for the ConvNet class evaluating the model
-#--------------------------------------------------------------------------------------
-model = ConvNet(input_size, hidden_size, num_classes, norm_layer=norm_layer).to(device)
+    # ======================================================================================
+    # Q1.a: Implementing convolutional neural net in PyTorch
+    # ======================================================================================
+    # In this question we will implement a convolutional neural networks using the PyTorch
+    # library.  Please complete the code for the ConvNet class evaluating the model
+    # --------------------------------------------------------------------------------------
+model = ConvNet(input_size, hidden_size, num_classes,
+                norm_layer=norm_layer).to(device)
 # Q2.a - Initialize the model with correct batch norm layer
 
 model.apply(weights_init)
 # Print the model
 print(model)
 # Print model size
-#======================================================================================
+# ======================================================================================
 # Q1.b: Implementing the function to count the number of trainable parameters in the model
-#======================================================================================
+# ======================================================================================
 PrintModelSize(model)
-#======================================================================================
+# ======================================================================================
 # Q1.a: Implementing the function to visualize the filters in the first conv layers.
 # Visualize the filters before training
-#======================================================================================
+# ======================================================================================
 VisualizeFilter(model)
 
 
 # Loss and optimizer
 criterion = nn.CrossEntropyLoss()
-optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate, weight_decay=reg)
+optimizer = torch.optim.Adam(
+    model.parameters(), lr=learning_rate, weight_decay=reg)
 
 # Train the model
 lr = learning_rate
@@ -196,9 +250,9 @@ for epoch in range(num_epochs):
         loss.backward()
         optimizer.step()
 
-        if (i+1) % 100 == 0:
+        if (i + 1) % 100 == 0:
             print ('Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}'
-                   .format(epoch+1, num_epochs, i+1, total_step, loss.item()))
+                   .format(epoch + 1, num_epochs, i + 1, total_step, loss.item()))
 
     # Code to update the lr
     lr *= learning_rate_decay
@@ -250,12 +304,11 @@ with torch.no_grad():
         if total == 1000:
             break
 
-    print('Accuracy of the network on the {} test images: {} %'.format(total, 100 * correct / total))
+    print('Accuracy of the network on the {} test images: {} %'.format(
+        total, 100 * correct / total))
 
 # Q1.c: Implementing the function to visualize the filters in the first conv layers.
 # Visualize the filters before training
 VisualizeFilter(model)
 # Save the model checkpoint
 torch.save(model.state_dict(), 'model.ckpt')
-
-
